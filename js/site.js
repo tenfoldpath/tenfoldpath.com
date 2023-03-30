628a3c6331350a732917e7bf062ee6d6b42ebb61
Dan Brubaker Horst <daniel.brubaker.horst@interfolio.com>
2023-03-29 21:43:22 -0400

    Pulling ServiceWorker entirely

--- js/site.js
+++ js/site.js
@@ -1,16 +1,1 @@
 document.documentElement.classList.remove("no-js");
-
-if (navigator.serviceWorker) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js', {
-      scope: '/'
-    }).then((reg) => {
-      console.log('Registered service worker', reg)
-    }).catch((err) => {
-      console.log('Service worker unavailable', err)
-    })
-    if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage({ command: 'trimCaches' })
-    }
-  })
-}
